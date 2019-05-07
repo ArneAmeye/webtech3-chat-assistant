7e7daf587bc14c023451528de29c7e0ec47c6365
Arne Ameye <arne.ameye@outlook.com>
2019-05-07 13:55:06 +0200

    added client side message.js, selected DOM elements for chat system

---
+++ public/javascripts/messages.js
@@ -0,0 +1,21 @@
+//CLIENT SIDE
+
+class Message{
+
+    constructor(){
+
+        //get chatbox input field
+        const messageInput = document.querySelector("#myMessage");
+        const messageBtn = document.querySelector(".btn--send");
+
+
+
+        //init primus websocket ###TODO###
+
+        //primus on , data listener ###TODO###
+
+    }
+
+}
+
+let message = new Message();
