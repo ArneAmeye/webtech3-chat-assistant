bd8f2e769302c41f3d0de6acaf64da2270f1bca5
Arne Ameye <arne.ameye@outlook.com>
2019-05-07 09:25:39 +0200

    init passport and passport session middleware

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//init passport.js and passport sessions middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
