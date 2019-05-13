9cc752f724e1a64a62723ce8d37ab40623560962
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-05-13 22:37:21 +0200

    saves webtoken in local storage on register and redirects

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport');
 const cors = require('cors');
+const passport = require('./passport/passport')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-let registerBtn = document.querySelector('.btn--login').addEventListener("click", ()=>{
+let registerBtn = document.querySelector('.btn--register').addEventListener("click", ()=>{
     let username = document.querySelector('#myUsername').value;
     let password = document.querySelector('#myPassword').value;
 
@@ -15,6 +15,36 @@ let registerBtn = document.querySelector('.btn--login').addEventListener("click"
         return response.json();
     }).then(json =>{
         if(json.status === "success"){
+            // let feedback = document.querySelector('.alert');
+            // feedback.textContent = "Register is complete!";
+            // feedback.classList.remove('hidden');
+
+            let token = json.data.token;
+            localStorage.setItem('token', token);
+            window.location.href = "http://localhost:3000";
+        }
+    })
+});
+
+let loginBtn = document.querySelector('.btn--login').addEventListener("click", ()=>{
+    let username = document.querySelector('#myUsername').value;
+    let password = document.querySelector('#myPassword').value;
+    console.log(username)
+    console.log(password)
+    fetch('http://localhost:3000/users/login', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response =>{
+        return response.json();
+    }).then(json =>{
+        if(json.status === "success"){
+            console.log("gelukt")
             // let feedback = document.querySelector('.alert');
             // feedback.textContent = "Register is complete!";
             // feedback.classList.remove('hidden');
