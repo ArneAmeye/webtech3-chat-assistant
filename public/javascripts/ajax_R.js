cadab8418c7e5745b2b301cde7734a6ca2b87eff
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-05-13 23:17:21 +0200

    fixed client-side bug
    een bug opgelost waardoor de ajax calls niet juist gebeurde

--- public/javascripts/ajax_R.js
+++ public/javascripts/ajax_R.js
@@ -25,29 +25,3 @@ let registerBtn = document.querySelector('.btn--register').addEventListener("cli
         }
     })
 });
-
-let loginBtn = document.querySelector('.btn--login').addEventListener("click", ()=>{
-    let username = document.querySelector('#myUsername').value;
-    let password = document.querySelector('#myPassword').value;
-    console.log(username)
-    console.log(password)
-    fetch('http://localhost:3000/users/login', {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "username": username,
-            "password": password
-        })
-    }).then(response =>{
-        return response.json();
-    }).then(json =>{
-        if(json.status === "success"){
-            console.log("gelukt")
-            // let feedback = document.querySelector('.alert');
-            // feedback.textContent = "Register is complete!";
-            // feedback.classList.remove('hidden');
-        }
-    })
-});
