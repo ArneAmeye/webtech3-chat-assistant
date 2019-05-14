a7b61f8d1523dbc3950e622c19bce8852fb9b666
Arne Ameye <arne.ameye@outlook.com>
2019-05-14 12:28:21 +0200

    adding error handling for incorrect login (user: false) and typo fixes

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,10 +30,23 @@ const signup = async (req,res,next)=>{
 const login = async (req,res,next) =>{
     
     const user = await User.authenticate()(req.body.username, req.body.password).then(result =>{
-        res.json({
+        console.log(result);
+        if(!result.user){
+            return res.json({
+                "status": "failed",
+                "message": "login failed"
+            })
+        }
+
+        let token = jwt.sign({
+            uid: result.user._id,
+            username: result.user.username
+        }, "MyVerySecretWord");
+
+        return res.json({
             "status": "success",
             "data": {
-                "user": result
+                "token": token
             }
         })
     }).catch(error => {
